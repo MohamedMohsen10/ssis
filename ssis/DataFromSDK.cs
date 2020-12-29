19e46e3a14af2fb1510a8991dcb67dff054657d4
Mohamed Mohsen <Mohamed.Mohsen@integrant.com>
2020-12-29 17:09:54 +0200

    Add project files.

---
+++ ssis/DataFromSDK.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssis
+{
+    public class DataFromSDK
+    {
+
+
+        public IList<WorkItem> GetAllWorkItems()
+        {
+
+
+            const String collectionUri = "https://projects.integrant.com/TFS/BICollection";
+            const String projectName = "TFS Reports";
+            const string token = "vxgwn6lrc6stdf52aem5stdx7hffmc4mt4epubpqcricso3ccmba";
+
+
+            //const String collectionUri = "https://dev.azure.com/IPTS-Perspecta";
+            //const String projectName = "IPTS";
+            //const string token = "gbjnuvypldsi4e2rvrk3a2xvahvgccfbzhh4tdjw2usdg6awufma";
+
+
+            var workItems = GetAllWorkItemsAsyc(collectionUri, projectName, token).Result;
+            return workItems;
+
+        }
+
+
+
+
+        public async Task<IList<WorkItem>> GetAllWorkItemsAsyc(string collectionUri, string project, string token)
+        {
+
+            var credentials = new VssBasicCredential(string.Empty, token);
+            List<int> ids = new List<int>();
+            // create a wiql object and build our query
+            var wiql = new Wiql()
+            {
+                // NOTE: Even if other columns are specified, only the ID & URL will be available in the WorkItemReference
+                Query = "Select [Id] " +
+                        "From WorkItems " +
+                        "Where [System.TeamProject] = '" + project + "'",
+            };
+
+            // create instance of work item tracking http client
+            using (WorkItemTrackingHttpClient httpClient = new WorkItemTrackingHttpClient(new Uri(collectionUri), credentials))
+            {
+                // execute the query to get the list of work items in the results
+                var result = await httpClient.QueryByWiqlAsync(wiql).ConfigureAwait(false);
+                int count = 0;
+                List<WorkItem> workItems = new List<WorkItem>();
+
+                //// build a list of the fields we want to see
+                //var fields = new[] { "System.Id", "System.Title", "System.State" ,"System.CommentCount",
+                //    "System.CreatedDate","System.CreatedBy","System.ChangedBy","System.Links.LinkType",
+                //     "Effort","Microsoft.VSTS.Scheduling.StoryPoints","Microsoft.VSTS.Scheduling.OriginalEstimate",
+                //     "Microsoft.VSTS.Scheduling.RemainingWork","Microsoft.VSTS.Scheduling.CompletedWork"
+
+
+                //};
+                foreach (var workItem in result.WorkItems)
+                {
+
+                    count++;
+                    ids.Add(workItem.Id);
+                    if (count >= 200)
+                    {
+                        //workItems.AddRange(await httpClient.GetWorkItemsAsync(ids, fields, result.AsOf).ConfigureAwait(false));
+                        workItems.AddRange(await httpClient.GetWorkItemsAsync(ids, expand: WorkItemExpand.All));
+                        count = 0;
+                        ids.Clear();
+                    }   
+
+                }
+
+                //ids.Add(66);
+                //workItems.AddRange(await httpClient.GetWorkItemsAsync(ids, fields, result.AsOf).ConfigureAwait(false));
+
+                return workItems;
+                // get work items for the ids found in query
+
+                //    return await httpClient.GetWorkItemsAsync(ids, fields, result.AsOf).ConfigureAwait(false);
+            }
+        }
+
+    }
+}
---
+++ ssis/Program.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Newtonsoft.Json;
+using ssis.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MongoDB.Driver;
+
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using System.Collections;
+
+namespace ssis
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+             //IList<WorkItem> workItems = GetAllWorkItems();
+            System.Text.StringBuilder stringbuilder = new System.Text.StringBuilder(String.Empty);
+
+            DataFromSDK dataFromSDK = new DataFromSDK();
+
+            IList<WorkItem> workItems = dataFromSDK.GetAllWorkItems();
+            #region BuildingStringforCSV
+            /*
+            foreach (WorkItem item in workItems)
+            {
+
+                stringbuilder.Append(item.Fields["System.Id"].ToString().Replace(",", "-") ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.Title"].ToString().Replace(",", "-") ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.State"].ToString().Replace(",", "-") ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.CommentCount"].ToString().Replace(",", "-") ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append(((IdentityRef)item.Fields["System.CreatedBy"]).Id.ToString().Replace(",", "-") ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append(((IdentityRef)item.Fields["System.ChangedBy"]).Id.ToString().Replace(",", "-") ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "System.Links.LinkType")?.FirstOrDefault().Value
+                    ?.ToString()?.Replace(",", "-")) ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Effort")?.FirstOrDefault().Value
+                    ?.ToString()?.Replace(",", "-")) ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.StoryPoints")?.FirstOrDefault().Value
+                    ?.ToString()?.Replace(",", "-")) ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.RemainingWork")?.FirstOrDefault().Value
+                   ?.ToString()?.Replace(",", "-")) ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.CompletedWork")?.FirstOrDefault().Value
+                                  ?.ToString()?.Replace(",", "-")) ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.OriginalEstimate")?.FirstOrDefault().Value
+                                   ?.ToString()?.Replace(",", "-")) ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Microsoft.VSTS.Common.Severity")?.FirstOrDefault().Value
+                                  ?.ToString()?.Replace(",", "-")) ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Microsoft.VSTS.Common.Priority")?.FirstOrDefault().Value
+                                  ?.ToString()?.Replace(",", "-")) ?? "0");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.WorkItemType"].ToString().Replace(",", "-") ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.TeamProject"].ToString().Replace(",", "-") ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.IterationPath"].ToString().Replace(",", "-") ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.Id"].ToString().Replace(",", "-") ?? "0"); //addsomething-------
+                stringbuilder.Append(",");
+                stringbuilder.Append((item?.Fields.Where(wi => wi.Key == "Microsoft.VSTS.Common.ValueArea")?.FirstOrDefault().Value
+                                  ?.ToString()?.Replace(",", "-")) ?? "NULL");
+                stringbuilder.Append(",");
+                stringbuilder.Append(item.Fields["System.IterationId"].ToString().Replace(",", "-") ?? "0");
+                stringbuilder.Append(",");
+
+                stringbuilder.Append(((IdentityRef)(item.Fields.Where(wi => wi.Key == "System.AssignedTo")
+                    .FirstOrDefault().Value))?.DisplayName?.ToString().Replace(",", "-") ?? "NULL");
+                //stringbuilder.Append(",");
+                stringbuilder.Append("|\n");
+            }*/
+            #endregion
+            #region CommentedRegion
+
+
+
+
+
+
+            //List<WorkItemModel> workItemModels = new List<WorkItemModel>();
+            //WorkItemModel workItemModel = new WorkItemModel();
+            //foreach (var item in workItems) {
+            //    workItemModel.SourceId = long.Parse(item.Fields["System.Id"].ToString());
+
+            //    workItemModel.State = item.Fields["System.State"].ToString().Replace(",", "-") ?? "NULL";
+
+            //    workItemModel.Title = item.Fields["System.Title"].ToString().Replace(",", "-") ?? "NULL";
+
+            //    workItemModel.CompletedWork = decimal.Parse(item?.Fields
+            //        .Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.CompletedWork")?.FirstOrDefault().Value.ToString());
+
+            //    workItemModel.OriginalEstimate = decimal.Parse(item?.Fields
+            //        .Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.OriginalEstimate")?.FirstOrDefault().Value.ToString());
+
+            //    workItemModel.RemainingWork = decimal.Parse(item?.Fields
+            //        .Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.RemainingWork")?.FirstOrDefault().Value.ToString());
+
+            //    workItemModel.Effort =decimal.Parse(item?.Fields.Where(wi => wi.Key == "Effort")?
+            //       .FirstOrDefault().Value.ToString());
+
+            //    workItemModel.StoryPoints = int.Parse(item.Fields
+            //        .Where(wi => wi.Key == "Microsoft.VSTS.Scheduling.StoryPoints")?.FirstOrDefault().Value.ToString());
+
+            //workItemModel.Severity = item?.Fields.Where(wi => wi.Key == "Severity")?
+            //   .FirstOrDefault().Value.ToString();
+
+            //workItemModel.Tags = workItemModel.Severity = item?.Fields.Where(wi => wi.Key == "Tags")?
+            //   .FirstOrDefault().Value.ToString();
+
+            //workItemModel.Priority = 
+
+
+            //workItemModels.Add(workItemModel);           
+            //}
+
+            //using (StreamWriter file = File.CreateText(@"C:\temp\WorkItems.JSON"))
+            //{
+            //    JsonSerializer serializer = new JsonSerializer();
+            //    //serialize object directly into file stream
+            //    //serializer.Serialize(file, JsonConvert.SerializeObject(workItems.Select(p => p.Fields)));
+            //    serializer.Serialize(file, Newtonsoft.Json.JsonConvert.SerializeObject(workItems.Select(p => p.Fields)));
+            //}
+
+            //System.IO.File.WriteAllText(@"C:\temp\WorkItems.csv", stringbuilder.ToString());
+            //Console.WriteLine("Done");
+
+            #endregion
+
+            var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+            IMongoDatabase db = dbClient.GetDatabase("Crops_MongoDB");
+            var BsonWorkItems = db.GetCollection<BsonDocument>("WorkItems");
+            Dictionary<string,object> tempDic = new Dictionary<string, object>();
+            var bsonDoc = new BsonDocument { };
+            foreach (var item in workItems)
+            {
+                foreach (var i in item.Fields)
+                {
+
+                    if (i.Value.GetType() != typeof(IdentityRef))
+                        tempDic.Add(i.Key.ToString().Replace("System", "").Replace(".", ""), i.Value);
+                    else
+                    {
+                        tempDic.Add(i.Key.ToString().Replace("System.", "").Replace(".", "") + "Id", ((IdentityRef)i.Value).Id);
+                        tempDic.Add(i.Key.ToString().Replace("System.", "").Replace(".", "") + "DisplayName", ((IdentityRef)i.Value).DisplayName);
+                    }
+                }
+                bsonDoc.AddRange(tempDic);
+                BsonWorkItems.InsertOne(bsonDoc);
+                tempDic.Clear();
+                bsonDoc.Clear();
+            }   
+            Console.WriteLine("Done");
+        }
+    }
+}
